4aeb70801ce5c35210d910d891fa547c87b97cf6
Rafael <sathler_754@hotmail.com>
2024-09-08 20:50:58 -0300

    adicionando atividade do INT

--- exercicios.py
+++ exercicios.py
@@ -3,8 +3,24 @@ import math
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+# num01 = int(input("Insira um numero inteiro: "))
+# num02 = int(input("Insira um numero inteiro: "))
+# print(num01 + num02)
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+# num01 = int(input("Insira um numero inteiro: "))
+# resto_divisao = num01 / 5
+# print(resto_divisao)
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+# num01 = int(input("Insira um numero inteiro: "))
+# num02 = int(input("Insira um numero inteiro: "))
+# resultado = num01 * num02
+# print(resultado)
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
 #numero_01 = int(input("Inserir um numero inteiro: "))
@@ -13,6 +29,10 @@ import math
 #print(resultado)
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+num01 = int(input("Insira um numero inteiro: "))
+resultado = num01 ** 2
+print(resultado)
 
 # #### Números de Ponto Flutuante (`float`)
 
@@ -33,11 +53,11 @@ import math
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
-data = input("Insira uma data no formato dd/mm/aaaa: ")
-lista_dia_mes_ano = data.split("/")
-print(f"O dia é: {lista_dia_mes_ano[0]}")
-print(f"O mês é: {lista_dia_mes_ano[1]}")
-print(f"O ano é: {lista_dia_mes_ano[2]}")
+# data = input("Insira uma data no formato dd/mm/aaaa: ")
+# lista_dia_mes_ano = data.split("/")
+# print(f"O dia é: {lista_dia_mes_ano[0]}")
+# print(f"O mês é: {lista_dia_mes_ano[1]}")
+# print(f"O ano é: {lista_dia_mes_ano[2]}")
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
