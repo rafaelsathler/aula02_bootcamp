f6370b2ffbea5a103b88f0ecf10c60e0dcced13d
Rafael <sathler_754@hotmail.com>
2024-09-12 20:55:06 -0300

    Adicionando exercicios booleanos

--- exercicios.py
+++ exercicios.py
@@ -99,19 +99,47 @@ import math
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
-texto01 = input("Inseir um texto: ")
-texto02 = input("Inserir um texto: ")
-concat = texto01 + texto02
-print(concat)
+#texto01 = input("Inseir um texto: ")
+#texto02 = input("Inserir um texto: ")
+#concat = texto01 + texto02
+#print(concat)
 
 
 # #### Booleanos (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+# x1 = False
+# x2 = True
+# resultado = x1 and x2
+# print(f"O resultado do AND é: {resultado}")
+
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+
+# x1 = False
+# x2 = True
+# resultado = x1 or x2
+# print(f"O resultado do OR é: {resultado}")
+
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+
+#  = False
+# resultado_not = not x1
+# print(f"O resultado do NOT é: {resultado_not}")
+
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+# x1 = float(input("Inserir um número: "))
+# x2 = float(input("Inserir um número: "))
+# resultado = x1 == x2
+# print(f"Os números são iguals? {resultado}")
+
 # 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+x1 = float(input("Inserir um número: "))
+x2 = float(input("Inserir um número: "))
+resultado = x1 != x2
+print(f"Os números são diferentes? {resultado}")
 
 # #### try-except e if
 
