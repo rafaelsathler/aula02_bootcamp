29219e008b8d01aa043fd7c3ab7f47b718cbb197
Rafael <sathler_754@hotmail.com>
2024-09-23 20:17:47 -0300

    adicionando desafio

--- desafio.py
+++ desafio.py
@@ -5,21 +5,29 @@ CONSTANTE_BONUS = 1000
 
 # nome_usuario = 33 isso e um erro?
 
-nome_usuario = input("Digite o seu nome: ")
+# nome_usuario = input("Digite o seu nome: ")
 
-if nome_usuario.isdigit():
-    print("Voce digitou seu nome errado")
-    exit()
-elif len(nome_usuario) == 0:
-    print("Voce nao digitou nada")
-    exit()
-elif nome_usuario.isspace():
-    print("Voce digitou so espaco")
-    exit()
+#if nome_usuario.isdigit():
+#    print("Voce digitou seu nome errado")
+#    exit()
+#elif len(nome_usuario) == 0:
+#    print("Voce nao digitou nada")
+#    exit()
+#elif nome_usuario.isspace():
+#    print("Voce digitou so espaco")
+#    exit()
 
 # 2) Solicita ao usuário que digite o valor do seu salário
 # Converte a entrada para um número de ponto flutuante
-salario_usuario = float(input("Digite o seu salario: "))
+
+#try:
+#    salario_usuario = float(input("Digite o seu salario: "))
+#    if salario_usuario < 0:
+#        print("Favor incluir algum valor positivo")
+#        exit()
+
+#except ValueError:
+#    print("Favor incluir valores numericos")
 
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
